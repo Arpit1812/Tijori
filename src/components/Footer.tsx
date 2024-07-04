c177290d45ba5d9e40ea5cda5be4cd4b36f62643
taaanviii <tanvisingh2004@gmail.com>
2024-07-04 16:19:39 +0530

    Update Footer.tsx
    added links to social media websites

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import IconButton from "@material-ui/core/IconButton";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import FacebookIcon from "@material-ui/icons/Facebook";
@@ -9,10 +11,38 @@ function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
-        <InstagramIcon />
-        <TwitterIcon />
-        <FacebookIcon />
-        <LinkedInIcon />
+        <IconButton
+          color="inherit"
+          component={Link}
+          to="https://www.instagram.com/"
+          target="_blank" // Open in a new tab
+        >
+          <InstagramIcon />
+        </IconButton>
+        <IconButton
+          color="inherit"
+          component={Link}
+          to="https://x.com/?lang=en"
+          target="_blank" // Open in a new tab
+        >
+          <TwitterIcon />
+        </IconButton>
+        <IconButton
+          color="inherit"
+          component={Link}
+          to="https://www.facebook.com/"
+          target="_blank" // Open in a new tab
+        >
+          <FacebookIcon />
+        </IconButton>
+        <IconButton
+          color="inherit"
+          component={Link}
+          to="https://www.linkedin.com/"
+          target="_blank" // Open in a new tab
+        >
+          <LinkedInIcon />
+        </IconButton>
       </div>
       <p> &copy; 2024 tijori</p>
     </div>
