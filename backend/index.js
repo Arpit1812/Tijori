70f03bcfe3d037e4ed0de5bbe6af2e3f888dbc96
Arpit1812 <99085527+Arpit1812@users.noreply.github.com>
2024-07-14 15:16:03 +0530

    Revert "minor updates"

--- backend/index.js
+++ backend/index.js
@@ -92,12 +92,10 @@
 
 const express = require('express');
 const { connectDB } = require('./db');
-// const uploadRoute = require('./routes/documents');
+const uploadRoute = require('./routes/documents');
 const authRoutes = require('./routes/auth');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const PdfSchema = require("./models/document")
-const multer = require('multer');
 
 const app = express();
 
@@ -113,35 +111,7 @@ connectDB();
 
 app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
-// app.use('/api/documents', uploadRoute);
-const storage = multer.diskStorage({
-  destination: function(req, file, cb){
-    cb(null, "./files")
-  },
-  filename: function(req, file, cb){
-    const uniqueSuffix = Date.now();
-    cb(null, uniqueSuffix + file.originalname);
-  }
-});
-
-const upload = multer({storage: storage});
-
-app.post("/upload-files", upload.single("file"), async(req, res) =>{
-  console.log(req.file)
-  const title = req.body.title
-  const filename = req.file.filename;
-
-  try{
-    await PdfSchema.create({tile: title, pdf: filename});
-    res.send({status: "ok"});
-  } catch (error){
-    res.json({status: error})
-  }
-}) 
-
-app.get("/upload", async(req, res) =>{
-  res.send("success!")
-} )
+app.use('/api/documents', uploadRoute);
 
 const PORT = 5000;
  app.listen(PORT, () => {
